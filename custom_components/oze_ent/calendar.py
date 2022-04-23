0b5d74ae3e24a272ed7a3db298ced798754d2812
lesensei <alain.perry@gmail.com>
2022-04-23 16:03:09 +0000

    Add a calendar entity for punishments

--- custom_components/oze_ent/calendar.py
+++ custom_components/oze_ent/calendar.py
@@ -18,15 +18,16 @@ async def async_setup_entry(hass, entry, async_add_devices):
     user_info = coordinator.data.get("user_info")
     devices = []
     for pupil in coordinator.api.userinfo.get_pupils_from_userinfo(user_info):
-        devices.append(OzeCalendarEntity(coordinator, pupil))
+        devices.append(ClassesCalendarEntity(coordinator, pupil))
+        devices.append(PunishmentCalendarEntity(coordinator, pupil))
     async_add_devices(devices)
 
 
-class OzeCalendarEntity(CalendarEntity):
-    """Oze Calendar entity."""
+class ClassesCalendarEntity(CalendarEntity):
+    """Oze classes calendar."""
 
     def __init__(self, oze: OzeEntity, pupil: dict[str, str]):
-        """Create the Oze Calendar Entity."""
+        """Create the Calendar Entity."""
         self.oze = oze
         self._event: CalendarEvent | None = None
         self._pupil = pupil
@@ -34,12 +35,12 @@ class OzeCalendarEntity(CalendarEntity):
     @property
     def unique_id(self) -> str | None:
         """Return a unique id for this sensor"""
-        return f"{DEFAULT_NAME}_{self._pupil['uid']}_{DOMAIN}"
+        return f"{DEFAULT_NAME}_{self._pupil['uid']}_classes"
 
     @property
     def name(self) -> str | None:
         """Return a descriptive name for this sensor"""
-        return f"{DEFAULT_NAME}_{self._pupil['first_name']}_{DOMAIN}"
+        return f"{DEFAULT_NAME}_{self._pupil['first_name']}_classes"
 
     @property
     def event(self) -> CalendarEvent | None:
@@ -53,7 +54,8 @@ class OzeCalendarEntity(CalendarEntity):
         )
         event_list: list[CalendarEvent] = []
         for event in events:
-            event_list.append(_get_calendar_event(event))
+            if event["_deletedStatus"] == 0:
+                event_list.append(_get_calendar_event(event))
         return event_list
 
     def update(self):
@@ -67,13 +69,64 @@ class OzeCalendarEntity(CalendarEntity):
         self._event = _get_calendar_event(events[0]) if len(events) > 0 else None
 
 
-def _get_calendar_event(event: dict[str, Any]) -> CalendarEvent:
-    """Return a HA CalendarEvent from an Oze event"""
+class PunishmentCalendarEntity(CalendarEntity):
+    """Oze punishment calendar."""
+
+    def __init__(self, oze: OzeEntity, pupil: dict[str, str]):
+        """Create the Calendar Entity."""
+        self.oze = oze
+        self._event: CalendarEvent | None = None
+        self._pupil = pupil
+
+    @property
+    def unique_id(self) -> str | None:
+        """Return a unique id for this sensor"""
+        return f"{DEFAULT_NAME}_{self._pupil['uid']}_punishments"
+
+    @property
+    def name(self) -> str | None:
+        """Return a descriptive name for this sensor"""
+        return f"{DEFAULT_NAME}_{self._pupil['first_name']}_punishments"
+
+    @property
+    def event(self) -> CalendarEvent | None:
+        """Return the next upcoming punishment event."""
+        return self._event
+
+    async def async_get_events(self, hass, start_date, end_date) -> list[CalendarEvent]:
+        """Get all classes in a specific time frame."""
+        punishments = await self.oze.api.punishment.get_punishments(
+            pupil=self._pupil, start_date=start_date, end_date=end_date
+        )
+        event_list: list[CalendarEvent] = []
+        for punishment in punishments:
+            if punishment["typePunition"]["needPeriode"]:
+                event_list.append(_get_calendar_event(punishment, True))
+        return event_list
+
+    def update(self):
+        """Update current event."""
+        events = self.oze.data.get(self._pupil["uid"]).get("punishments")
+        nowtime = datetime.now(tz.gettz("Europe/Paris"))
+        events = list(
+            filter(lambda event: nowtime <= parser.parse(event["dateFin"]), events)
+        )
+        events.sort(key=lambda event: event["dateDebut"])
+        self._event = _get_calendar_event(events[0], True) if len(events) > 0 else None
+
+
+def _get_calendar_event(
+    event: dict[str, Any], isPunishment: bool = False
+) -> CalendarEvent:
+    """Return a HA CalendarEvent from an Oze class or punishment"""
+    prof = event["profs"][0] if not isPunishment else event["responsableSuivi"]
     return CalendarEvent(
-        summary=event["matieres"][0]["libelle"],
+        summary=event["matieres"][0]["libelle"]
+        if not isPunishment
+        else event["typePunition"]["libelle"],
         start=dt.parse_datetime(event["dateDebut"]),
         end=dt.parse_datetime(event["dateFin"]),
-        description=event["profs"][0]["nom"] + " " + event["profs"][0]["prenom"],
+        description=prof["nom"] + " " + prof["prenom"],
         location=event["salles"][0]["libelle"]
         if "salles" in event and event["salles"]
         else "",
